20092077f9ae20feb611d9ec8d783dbc34fea5bc
Pavel_Vynychenko <41605550+PavelVyny@users.noreply.github.com>
2020-03-18 03:29:39 +0200

    project structure preparation
    - cleaning useless files and folders;
    - adding sass preprocessor;
    - add new favicon;

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+
+        <main className="wrapper">
+        <section className="tracker-map">
+
+        </section>
+       
+        <aside className="tracker-aside">
+
+        </aside>
+      </main>
+
+    );
+  }
+}
+
+export default App;
