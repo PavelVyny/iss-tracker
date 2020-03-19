ee48328ffeb13a70aefbeed7c79730d30cc2a2dd
Pavel_Vynychenko <41605550+PavelVyny@users.noreply.github.com>
2020-03-19 14:25:36 +0200

    limiting output of decimal places
    put toFixed method to the right place

--- src/components/CurrentLocation.js
+++ src/components/CurrentLocation.js
@@ -5,8 +5,8 @@ const CurrentLoc = ({coords}) => {
 		<div className="tracker-location">
 			<h4 className="tracker-location__name">ISS is now located at:</h4>
 			<div className="tracker-location__values">
-				<p className="tracker-location__lng">longitude: {coords.lng}</p>
-				<p className="tracker-location__lat">latitude: {coords.lat}</p>
+				<p className="tracker-location__lng">longitude: {coords.lng.toFixed(3)}</p>
+				<p className="tracker-location__lat">latitude: {coords.lat.toFixed(3)}</p>
 			</div>
 		</div>
 	) 
