66cbd353934396a9a8e4647fc89f88e44f8c6e7e
Pavel_Vynychenko <41605550+PavelVyny@users.noreply.github.com>
2020-03-19 05:13:20 +0200

    added builded app

---
+++ build/precache-manifest.cdfd84e2588edbbbd0f39fd23954aeb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9cd311b64ab0664fb4517df1e210e0",
+    "url": "/iss-tracker/index.html"
+  },
+  {
+    "revision": "e72ffe80923d0acb23e5",
+    "url": "/iss-tracker/static/css/main.ab14ab2f.chunk.css"
+  },
+  {
+    "revision": "41d960060ec9392bfe11",
+    "url": "/iss-tracker/static/js/2.c5203829.chunk.js"
+  },
+  {
+    "revision": "577ef60f73636c6e95b316a24bc4f83f",
+    "url": "/iss-tracker/static/js/2.c5203829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72ffe80923d0acb23e5",
+    "url": "/iss-tracker/static/js/main.3f7add26.chunk.js"
+  },
+  {
+    "revision": "c436901be601fa7ab22c",
+    "url": "/iss-tracker/static/js/runtime-main.eb3f9b67.js"
+  },
+  {
+    "revision": "be3b6d4c35819850ad20970331ff4fcf",
+    "url": "/iss-tracker/static/media/astronaut.be3b6d4c.png"
+  },
+  {
+    "revision": "639b29e73f07307f5677a39970fd4c4a",
+    "url": "/iss-tracker/static/media/marker.639b29e7.png"
+  }
+]);
